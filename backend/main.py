88d86e09d428d79ca67fafd19ed7efda99e042aa
wolfmindasd-stack <wolfmind.asd@gmail.com>
2026-09-21 21:59:47 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # Importa i router del tuo backend
 from backend.server import router as server_router
-from auth_utils import router as auth_router
+from backend.auth_utils import router as auth_router
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
