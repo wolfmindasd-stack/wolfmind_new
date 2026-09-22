5ddc2874cc224ca24fd550ab5180290e7a436a58
wolfmindasd-stack <wolfmind.asd@gmail.com>
2026-09-23 01:42:25 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -74,9 +74,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://wolfmind-new.pages.dev",
-        "https://bfc432bf.wolfmind-new.pages.dev"
-    ],
+    "https://wolfmind-new.pages.dev",
+    "https://bfc432bf.wolfmind-new.pages.dev",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
